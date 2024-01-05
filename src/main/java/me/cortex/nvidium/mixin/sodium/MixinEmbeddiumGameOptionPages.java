1ce43682104aa78f6fd15ff333749196dab96c02
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2024-01-05 23:09:03 +0100

    disable embeddium options

---
+++ src/main/java/me/cortex/nvidium/mixin/sodium/MixinEmbeddiumGameOptionPages.java
@@ -0,0 +1,22 @@
+package me.cortex.nvidium.mixin.sodium;
+
+import me.cortex.nvidium.Nvidium;
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
+import me.jellysquid.mods.sodium.client.gui.SodiumGameOptionPages;
+import me.jellysquid.mods.sodium.client.gui.options.OptionGroup;
+import me.jellysquid.mods.sodium.client.gui.options.OptionImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(SodiumGameOptionPages.class)
+public class MixinEmbeddiumGameOptionPages {
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lme/jellysquid/mods/sodium/client/gui/options/OptionImpl$Builder;build()Lme/jellysquid/mods/sodium/client/gui/options/OptionImpl;",
+            ordinal = 2), method = "advanced", remap = false)
+    private static OptionImpl faceculling(OptionImpl.Builder builder) {
+        builder.setEnabled(!Nvidium.IS_ENABLED);
+        SodiumClientMod.options().performance.useTranslucentFaceSorting = false;
+        return builder.build();
+    }
+}
