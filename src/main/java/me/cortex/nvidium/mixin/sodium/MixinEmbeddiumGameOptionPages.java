b96c3cfba21aa02896700d425a8e67a8850e930e
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2024-01-06 15:42:59 +0100

    Update MixinEmbeddiumGameOptionPages.java

--- src/main/java/me/cortex/nvidium/mixin/sodium/MixinEmbeddiumGameOptionPages.java
+++ src/main/java/me/cortex/nvidium/mixin/sodium/MixinEmbeddiumGameOptionPages.java
@@ -3,7 +3,6 @@ package me.cortex.nvidium.mixin.sodium;
 import me.cortex.nvidium.Nvidium;
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import me.jellysquid.mods.sodium.client.gui.SodiumGameOptionPages;
-import me.jellysquid.mods.sodium.client.gui.options.OptionGroup;
 import me.jellysquid.mods.sodium.client.gui.options.OptionImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +15,8 @@ public class MixinEmbeddiumGameOptionPages {
             ordinal = 2), method = "advanced", remap = false)
     private static OptionImpl faceculling(OptionImpl.Builder builder) {
         builder.setEnabled(!Nvidium.IS_ENABLED);
-        SodiumClientMod.options().performance.useTranslucentFaceSorting = false;
+        if (Nvidium.IS_ENABLED)
+            SodiumClientMod.options().performance.useTranslucentFaceSorting = false;
         return builder.build();
     }
 }
