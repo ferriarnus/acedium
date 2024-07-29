42dd828cc4d273f40fd966d4cc675608a57240be
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2024-07-29 17:40:51 +0200

    Update MixinRenderRegionManager.java

--- src/main/java/me/cortex/nvidium/mixin/sodium/MixinRenderRegionManager.java
+++ src/main/java/me/cortex/nvidium/mixin/sodium/MixinRenderRegionManager.java
@@ -21,15 +21,26 @@ public abstract class MixinRenderRegionManager implements INvidiumWorldRendererS
 
     @Shadow protected abstract void uploadMeshes(CommandList commandList, RenderRegion region, Collection<ChunkBuildOutput> results);
 
+    @Shadow protected abstract void uploadResorts(CommandList commandList, RenderRegion region, Collection<ChunkBuildOutput> results);
+
     @Unique private NvidiumWorldRenderer renderer;
 
 
-    @Redirect(method = "uploadMeshes(Lorg/embeddedt/embeddium/impl/gl/device/CommandList;Ljava/util/Collection;)V", at = @At(value = "INVOKE", target = "Lorg/embeddedt/embeddium/impl/render/chunk/region/RenderRegionManager;uploadResorts(Lorg/embeddedt/embeddium/impl/gl/device/CommandList;Lorg/embeddedt/embeddium/impl/render/chunk/region/RenderRegion;Ljava/util/Collection;)V"))
+    @Redirect(method = "uploadMeshes(Lorg/embeddedt/embeddium/impl/gl/device/CommandList;Ljava/util/Collection;)V", at = @At(value = "INVOKE", target = "Lorg/embeddedt/embeddium/impl/render/chunk/region/RenderRegionManager;uploadMeshes(Lorg/embeddedt/embeddium/impl/gl/device/CommandList;Lorg/embeddedt/embeddium/impl/render/chunk/region/RenderRegion;Ljava/util/Collection;)V"))
     private void redirectUpload(RenderRegionManager instance, CommandList cmdList, RenderRegion pass, Collection<ChunkBuildOutput> uploadQueue) {
         if (Nvidium.IS_ENABLED) {
             uploadQueue.forEach(renderer::uploadBuildResult);
         } else {
             uploadMeshes(cmdList, pass, uploadQueue);
+        }
+    }
+
+    @Redirect(method = "uploadMeshes(Lorg/embeddedt/embeddium/impl/gl/device/CommandList;Ljava/util/Collection;)V", at = @At(value = "INVOKE", target = "Lorg/embeddedt/embeddium/impl/render/chunk/region/RenderRegionManager;uploadResorts(Lorg/embeddedt/embeddium/impl/gl/device/CommandList;Lorg/embeddedt/embeddium/impl/render/chunk/region/RenderRegion;Ljava/util/Collection;)V"))
+    private void redirectTS(RenderRegionManager instance, CommandList cmdList, RenderRegion pass, Collection<ChunkBuildOutput> uploadQueue) {
+        if (Nvidium.IS_ENABLED) {
+            //uploadQueue.forEach(renderer::uploadBuildResult);
+        } else {
+            uploadResorts(cmdList, pass, uploadQueue);
         }
     }
 
